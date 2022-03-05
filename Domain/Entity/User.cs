46048e3705f0f921f93165eb9afcbefd76a746a3
小赖 <422068923@qq.com>
2022-03-05 13:50:28 +0800

    添加项目文件。

---
+++ Domain/Entity/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Demo.Domain.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string  Account { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Domain/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Login.Demo.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Demo.Domain
+{
+    public class MyDbContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public MyDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+    }
+}
