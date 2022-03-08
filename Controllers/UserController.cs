5832762bcfd4ba6803c4fc2f5270ce52cd448cc7
小赖 <422068923@qq.com>
2022-03-08 22:48:21 +0800

    1232

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Login.Demo.Controllers
 {
     public class UserController : Controller
-    {
+    {  
         public IActionResult Index()
         {
             return View();
